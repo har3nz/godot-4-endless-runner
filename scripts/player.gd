e02b29e8f8d6d6e16ea4ceeae50c0183981b1948
har3nz <ekremfoto115@gmail.com>
2023-12-30 15:42:41 +0300

    added initial files

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 420.0
+const JUMP_VELOCITY = -590.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var animation_player
+
+func _physics_process(delta):
+	animation_player = $player/AnimationPlayer  # Replace "AnimationPlayer" with the actual path to your AnimationPlayer node
+	animation_player.play("run")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Move to the right continuously by incrementing x position.
+	position.x += SPEED * delta
+	move_and_slide()
+	
---
+++ scripts/world.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var obstacle = preload("res://scenes/tilemap.tscn")
+
+var max_obstacle = 1000
+
+func _ready() -> void:
+	for i in range(max_obstacle):
+		var Obstacle_scene = obstacle.instantiate()
+		Obstacle_scene.global_position = Vector2(1582 *(i+1), -287)
+		$Obstacle_container.add_child(Obstacle_scene)
